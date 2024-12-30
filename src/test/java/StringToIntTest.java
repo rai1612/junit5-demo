bf410481ae3ebd8625c00a3a9f745f3f56a93da7
Arun Kumar Rai <raia41145@gmail.com>
2024-12-30 21:22:16 +0530

    @DisplayName example

--- src/test/java/StringToIntTest.java
+++ src/test/java/StringToIntTest.java
@@ -1,15 +1,19 @@
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+@DisplayName(value = "string to integer testing")
 public class StringToIntTest {
 
     String s;
     @Test
+    @DisplayName(value = "illegal argument exception test")
     void testConvertThrowsIllegalArgumentException(){
         s = "";
         Assertions.assertThrows(IllegalArgumentException.class, () -> StringToInt.convert(s));
     }
     @Test
+    @DisplayName(value = "number format exception test")
     void testConvertThrowsNumberFormatException(){
         s = "234sd";
         Assertions.assertThrows(NumberFormatException.class, ()-> StringToInt.convert(s));
